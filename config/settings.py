bea9415c1c89246780f2f3fd9fb5cd9169e7f59e
IrradaZholman <164073065+IrradaZholman@users.noreply.github.com>
2026-04-29 10:12:14 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -77,6 +77,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'assets']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
